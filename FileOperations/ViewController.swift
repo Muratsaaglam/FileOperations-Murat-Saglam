37fbb8b4f989dd91212bebc1d0cd09c68991cff3
Muratsaaglam <muratsaaglam@outlook.com>
2022-09-19 21:39:10 +0300

    Initial Commit

---
+++ FileOperations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileOperations
+//
+//  Created by Murat Sağlam on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
